b4e1819b0e3c3b950e472bf5339d6b6cac247cc9
Nyle Rodgers <nyle.rodgers@gmail.com>
2016-08-01 13:41:28 -0400

    condense files

--- process_data.py
+++ process_data.py
@@ -1,6 +1,3 @@
-from process_results import (format_results,
-                             correct_pvalues_for_multiple_testing)
-
 import logging
 
 from email_results import send_error_as_email, send_results_as_email
@@ -84,3 +81,48 @@ def core_otus(params, inputs, cfg):
                    if (interest_presence_fracs[otu] >= frac and
                        out_presence_fracs[otu] < frac)]
             for frac in run_config.FRACS}
+
+
+def format_results(res, params, cfg):
+    attachments = list()
+    sign_results = (('OTU\tpval\t%s ' +
+                     'corrected pval\tthreshold\n')
+                    % params['p_val_adj'])
+    for frac in reversed(sorted(res.keys())):
+        for otu in res[frac]:
+            sign_results += '%s\t%s\t%s\t%s\n' % (
+                otu['otu'], otu['pval'],
+                otu['corrected_pval'], int(frac * 100))
+    attachments.append(('%s_results_%s.tsv' % (cfg['name'], params['name']),
+                        sign_results))
+    print sign_results
+    return attachments
+
+
+def correct_pvalues_for_multiple_testing(pvalues, correction_type):
+    n = len(pvalues)
+    if correction_type == 'bf':  # Bonferroni
+        new_pvalues = [n * p for p in pvalues]
+    elif correction_type == 'bf-h':  # Bonferroni-Holm
+        values = sorted([(p, i) for i, p in enumerate(pvalues)])
+        new_pvalues = [None] * n
+        for rank, (p, i) in enumerate(values):
+            new_pvalues[i] = (n - rank) * p
+    elif correction_type == 'b-h':  # Benjamini-Hochberg
+        values = list(reversed(sorted(
+            [(p, i) for i, p in enumerate(pvalues)]
+        )))
+        adjusted_vals = [float(n)/(n - i) * p
+                         for i, (p, index) in enumerate(values)]
+        for i in xrange(n - 1):
+            if adjusted_vals[i] < adjusted_vals[i + 1]:
+                adjusted_vals[i + 1] = adjusted_vals[i]
+        new_pvalues = [None] * n
+        for i, (p, index) in enumerate(values):
+            new_pvalues[index] = adjusted_vals[i]
+    elif correction_type == 'none':
+        new_pvalues = pvalues[:]
+    else:
+        logging.warn('Invalid correction type of "%s" given to correct_pvalues'
+                     % correction_type)
+    return new_pvalues
